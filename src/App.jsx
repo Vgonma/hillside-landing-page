6a100693e3f53bddeb76e0129cbd1b73e2ec6208
Vgonma <victorgmassimi.microverse@gmail.com>
2023-04-04 16:01:54 -0600

    footer sticks to the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 import Home from './components/home';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Navbar></Navbar>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='nosotros' />
         <Route path='*' element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
