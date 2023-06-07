36b48515ce2b2a7f3f277326517e70ab912661fe
Vgonma <victorgmassimi.microverse@gmail.com>
2023-06-07 16:40:34 -0600

    nosotros base complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Home from './components/home';
 import Levels from './components/levels'
+import Nosotros from './components/nosotros';
+import Contact from './components/contact';
 import './App.css';
 
 function App() {
@@ -12,8 +14,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='nosotros' />
+        <Route path='nosotros' element={<Nosotros />} />
         <Route path='niveles' element={<Levels />} />
+        <Route path='contacto' element={<Contact />} />
         <Route path='*' element={<Home />} />
       </Routes>
       <Footer />
