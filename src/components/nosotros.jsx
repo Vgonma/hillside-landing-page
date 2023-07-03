35ba56d03e059229743fdfbd2d5027a8b4dfb727
Vgonma <victorgmassimi.microverse@gmail.com>
2023-07-03 14:04:48 -0600

    Updated nosotros text

--- src/components/nosotros.jsx
+++ src/components/nosotros.jsx
@@ -8,7 +8,6 @@ import teachers from '../assets/images/photos/personal.jpg';
 import nuestro_sistema from '../assets/images/titles/NUESTRO-SISTEMA.svg';
 import sistemaImage from '../assets/images/photos/sistema.jpg';
 
-
 function Nosotros() {
   return (
     <>
@@ -17,23 +16,16 @@ function Nosotros() {
           <div className='nosotros-hero-left-text'>
             <img src={NosotrosTitle} alt='Nosotros' className='' />
             <p>
-              Hillside School fue fundado hace 26 años por Martha Saenz
+              Hillside School fue fundado hace 26 años por Martha Sáenz
               Castillo, educadora y maestra en Estados Unidos y México, quien al
               estudiar el método Constructivista decidió abrir su propio kinder
               100% bilingüe y proponer una nueva manera de enseñanza en México.
               Con el paso de los años su filosofía educativa fue evolucionando
               hasta convertirse en lo que es hoy, un programa que consta de la
               fusión de los mejores métodos de aprendizaje, tales como Waldorf,
-              Montesori y Tradicional.
+              Montessori Constructivista y Tradicional.
             </p>
 
-            <p>
-              En Hillside School proponemos un ambiente equilibrado entre el
-              desarrollo de las habilidades académicas y el crecimiento
-              socioemocional. Nuestra finalidad siempre ha sido darles a
-              nuestros alumnos las herramientas necesarias para crecer felices,
-              seguros y preparados para el futuro.
-            </p>
             <Link to='/contacto' className='band-button bg-green'>
               <span>AGENDA UNA VISITA!</span>
             </Link>
@@ -48,7 +40,7 @@ function Nosotros() {
       </section>
 
       <section className='home-section nuestro-sistema' id='sistema'>
-      <div className='sistema-1'>
+        <div className='sistema-1'>
           <img src={nuestro_sistema} alt='Nuestro Sistema' />
           <strong>Sistema constructivista</strong>
           <p>alumno como elemento central</p>
@@ -71,8 +63,6 @@ function Nosotros() {
           />
         </div>
       </section>
-
-      
     </>
   );
 }
