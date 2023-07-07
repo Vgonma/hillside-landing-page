bf3cd1daddd03565d53a561ab9db6d28a2791ac6
Vgonma <victorgmassimi.microverse@gmail.com>
2023-07-06 18:46:52 -0600

    updated homepage hero images

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import play from '../assets/images/photos/hillside_play.jpg';
-import grow from '../assets/images/photos/hillside_grow.jpeg';
-import learn from '../assets/images/photos/hillside_learn.jpg';
+import hero1 from '../assets/images/photos/hero-1.jpeg';
+import hero2 from '../assets/images/photos/hero-2.jpeg';
+import hero3 from '../assets/images/photos/hero-3.jpg';
+import hero4 from '../assets/images/photos/hero-4.jpg';
+import hero5 from '../assets/images/photos/hero-5.jpg';
+
 import skills4life from '../assets/images/titles/SKILLS-FOR-LIFE.svg';
 
 function Hero() {
   const [image, setImage] = useState(0);
-  const wordList = ['LeaRn!', 'PlaY!', 'GrOw!'];
-  const imageList = [learn, play, grow];
+  const imageList = [hero1, hero2, hero3, hero4, hero5];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImage((image + 1) % 3);
-    }, 4000);
+      setImage((image + 1) % 5);
+    }, 2500);
   
     return () => {
       clearInterval(interval);
